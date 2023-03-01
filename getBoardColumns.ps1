7e5d357b49b371c634d7043bc442d8a59b14c752
BasicByte <31377905+Mortos98@users.noreply.github.com>
2023-03-01 00:49:44 +0000

    pat

--- getBoardColumns.ps1
+++ getBoardColumns.ps1
@@ -1,22 +1,21 @@
 #invoke a rest get request for the api create a get request to call https://dev.azure.com/TGAAU/_apis/projects/Aged Care Projects/teams?api-version=7.0
 #this will return a list of teams in the project
 
-$cred = Get-Credential
+$PAT = "xxxxxxxxxxxxxxxxxxxx"
 $uri = "https://dev.azure.com/TGAAU/_apis/projects/Aged Care Projects/teams?api-version=7.0"
 $uri = [System.Uri]::EscapeUriString($uri)
 $uri = [System.Uri]::new($uri)
 $uri = $uri.AbsoluteUri
 $uri = [System.Uri]::new($uri)
 
+$cred = New-Object System.Management.Automation.PSCredential("PAT", (ConvertTo-SecureString $PAT -AsPlainText -Force))
 $body = @{
-    "uri" = $uri
-    "method" = "GET"
-    "headers" = @{
-        "Content-Type" = "application/json"
-    }
-    "body" = ""
+    Method = 'Get'
+    Uri = $uri
+    ContentType = 'application/json'
+    UseDefaultCredentials = $false
+    Credential = $cred
 }
-
 #list returned teams
 $teams = Invoke-RestMethod @body -Credential $cred
 $teams.value | select customDisplayName
