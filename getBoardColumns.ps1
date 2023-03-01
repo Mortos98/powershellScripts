5026e16aaf6dbd45084be79cf58085ecfe9a090c
BasicByte <31377905+Mortos98@users.noreply.github.com>
2023-03-01 23:57:39 +0000

    get board levels

--- getBoardColumns.ps1
+++ getBoardColumns.ps1
@@ -7,15 +7,27 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$PersonalAccessToken
 )
-
-# create a get request for https://dev.azure.com/TGAAU/_apis/projects/Aged Care Projects/teams?api-version=7.0 with PAT Token
-$uri = "https://dev.azure.com/$OrganizationName/_apis/projects/$ProjectName/teams?api-version=7.0"
-# escape the uri string
-$uri = [System.Uri]::EscapeUriString($uri)
 # encrypt a blank string and the  PersonalAccessToken with base 64
 $encodedCredentials = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes((":$PersonalAccessToken")))
 
-#invoke the rest call
-$teams = Invoke-RestMethod -Uri $uri -Methon Get -ContentType "application/json; charset=utf-8; api-version=7.0" -Headers @{"Authorization"="Basic $encodedCredentials"}
+# create a get request for https://dev.azure.com/TGAAU/_apis/projects/Aged Care Projects/teams?api-version=7.0 with PAT Token
+$uri = "https://dev.azure.com/$OrganizationName/_apis/projects/$ProjectName/teams?api-version=7.0"
+$uri = [System.Uri]::EscapeUriString($uri)
 
-Write-Output $teams
+#invoke the rest call
+$teams = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json; charset=utf-8; api-version=7.0" -Headers @{"Authorization"="Basic $encodedCredentials"}
+# loop through teams and get team ids and names and create an object with the team name and id
+foreach ($team in $teams.value) {
+    $teamId = $team.id
+    $teamName = $team.name
+
+    $uriboards = "https://dev.azure.com/$OrganizationName/$projectName/$teamId/_apis/work/backlogs?api-version=7.0"
+    $uriboards = [System.Uri]::EscapeUriString($uriboards)
+    $boards = Invoke-RestMethod -Uri $uriboards -Method Get -ContentType "application/json; charset=utf-8; api-version=7.0" -Headers @{"Authorization"="Basic $encodedCredentials"}
+
+    $teamObject = New-Object -TypeName PSObject -Property @{
+        TeamName = $teamName
+        TeamId = $teamId
+    }
+    $teamObject | Select-Object TeamName, TeamId
+}
